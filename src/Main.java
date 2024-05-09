5497dad4b8031b959fb30a3529a1f64c6cb12ba2
OmidrezaBagheriyan <OmidrezaBagheriyan@gmail.com>
2024-05-09 13:15:30 -0700

    OmidrezaBagheriyan\JavaWorking - CardVisit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,38 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
+
+    String fullName, job, tel, email;
+
     public static void main(String[] args) {
-        System.out.println("Hello, My name is Omidreza Bagheriyan.");
+        Main main = new Main();
+        main.setup();
+    }
+
+    void setup() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter full name:");
+        String fullName = sc.nextLine();
+        System.out.println("Please enter job:");
+        String job = sc.nextLine();
+        System.out.println("Please enter tel:");
+        String tel = sc.nextLine();
+        System.out.println("Please enter email:");
+        String email = sc.nextLine();
+        input(fullName, job, tel, email);
+        sc.close();
+        System.out.println("\nYour card visit");
+        System.out.println(showInputs());
+    }
+
+    void input(String fullName, String job, String tel, String email) {
+        this.fullName = fullName;
+        this.job = job;
+        this.tel = tel;
+        this.email = email;
+    }
+
+    String showInputs() {
+        return String.format("%1$s\n%2$s\n%3$s\n%4$s", this.fullName, this.job, this.tel, this.email);
     }
 }
